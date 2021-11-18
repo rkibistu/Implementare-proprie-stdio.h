c8e6825f673ece0860320c75cb90c71844d2436f
rkibistu <ogreanrazvan@gmail.com>
2021-11-18 14:53:38 +0200

    nimic inca

---
+++ so_stdio.c
@@ -0,0 +1,23 @@
+#include "so_stdio.h"
+
+#define TRUE 1
+#define FALSE 0
+
+struct _so_file {
+
+	//ptr cursor
+	size_t _readPtr;
+	size_t _writePtr;
+
+
+	//flaguri
+	int 
+
+
+	//buffere
+		//read buffer
+		//write buffer
+
+	//pathname ??
+};
+
